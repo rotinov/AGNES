c0a4e412c7ff95737a89cbd196fd888f54e544ba
Rotinov Egor <rotinov-github@pm.me>
2020-02-09 16:01:17 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -33,16 +33,3 @@ setuptools.setup(name='agnes',
                      "Operating System :: OS Independent",
                  ]
                  )
-
-
-import pkg_resources
-
-tf_pkg = None
-for tf_pkg_name in ['torch']:
-    try:
-        tf_pkg = pkg_resources.get_distribution(tf_pkg_name)
-    except pkg_resources.DistributionNotFound:
-        pass
-assert tf_pkg is not None, 'PyTorch needed, of version above 1.0.0'
-from distutils.version import LooseVersion
-assert LooseVersion(re.sub(r'-?rc\d+$', '', tf_pkg.version)) >= LooseVersion('1.0.0')
